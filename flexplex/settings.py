a3d5b172efc76b151886597e8dfd93474171e01b
rushi_t04 <rushikesh.talokar@gmail.com>
2017-07-16 19:22:06 +0530

    - fresh db migrations

--- flexplex/settings.py
+++ flexplex/settings.py
@@ -104,12 +104,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'flexplex',
         'USER': 'root',
-        'PASSWORD': 'root',
+        'PASSWORD': 'Rushi_123',
         'HOST': '127.0.0.1',
-        'PORT': '3306',
-        # 'PASSWORD': 'Rushi_123',
-        # 'HOST': '127.0.0.1',
-        # 'PORT': '3306',
+        'PORT': '3306'
     }
 }
 #Admin: rtalokar
